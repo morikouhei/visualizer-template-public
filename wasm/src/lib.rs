ccdeacd717094a4136e58f63895d6721d494daa3
morikouhei <morikouhe139@gmail.com>
2024-04-16 22:06:52 +0900

    new commit for AHC026

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,6 +4,7 @@ mod util;
 #[wasm_bindgen]
 pub fn gen(seed: i32) -> String {
     util::gen(seed as u64).to_string()
+    // String::new()
 }
 
 #[wasm_bindgen(getter_with_clone)]
@@ -13,11 +14,25 @@ pub struct Ret {
     pub svg: String,
 }
 
+
 #[wasm_bindgen]
 pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
     let input = util::parse_input(&_input);
-    let output = util::parse_output(&_output);
-    let (score, err, svg) = util::vis(&input, &output, turn);
+    let score = turn;
+    
+    let output = util::parse_output(&input,&_output);
+
+    // Ret {
+    //     score: score as i64,
+    //     err: String::new(),
+    //     svg: String::new(),
+
+    // }
+    let (score, err, svg) = match output {
+        Ok(output) => util::vis(&input, &output, turn),
+        Err(err) => (0, err, String::new()),
+    };
+
     Ret {
         score: score as i64,
         err,
@@ -27,6 +42,13 @@ pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
 
 #[wasm_bindgen]
 pub fn get_max_turn(_input: String, _output: String) -> usize {
-    let output = util::parse_output(&_output);
-    output.q
+    let input = util::parse_input(&_input);
+
+    let result = util::parse_output(&input, &_output);
+
+    match result {
+        Ok(output) => output.out.len(),
+        Err(_) => 0,
+    }
+    
 }
--- wasm/src/util.rs
+++ wasm/src/util.rs
@@ -1,9 +1,15 @@
 #![allow(non_snake_case, unused_macros)]
+
+use itertools::{max, Itertools};
 use proconio::input;
 use rand::prelude::*;
-use std::collections::VecDeque;
-use svg::node::element::{Rectangle, Style};
-use web_sys::console::log_1;
+// use std::fmt::format;
+use std::{collections::BTreeSet, ops::RangeBounds};
+// use std::ops::RangeBounds;
+use svg::node::element::{Rectangle, Style,Line,Text,Circle, Title,Group};
+
+use svg::node::Text as TextContent;
+// use svg::Document;
 
 pub trait SetMinMax {
     fn setmin(&mut self, v: Self) -> bool;
@@ -27,213 +33,34 @@ where
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct Input {
-    pub id: usize,
-    pub n: usize,
-    pub k: usize,
-    pub s: Vec<String>,
+#[macro_export]
+macro_rules! mat {
+	($($e:expr),*) => { Vec::from(vec![$($e),*]) };
+	($($e:expr,)*) => { Vec::from(vec![$($e),*]) };
+	($e:expr; $d:expr) => { Vec::from(vec![$e; $d]) };
+	($e:expr; $d:expr $(; $ds:expr)+) => { Vec::from(vec![mat![$e $(; $ds)*]; $d]) };
 }
 
-impl std::fmt::Display for Input {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{} {} {}", self.id, self.n, self.k)?;
-        for i in 0..self.n {
-            writeln!(f, "{}", self.s[i])?;
-        }
-        Ok(())
-    }
-}
-
-pub fn parse_input(f: &str) -> Input {
-    let f = proconio::source::once::OnceSource::from(f);
-    input! {
-        from f,
-        id:usize,
-        n: usize,
-        k: usize,
-        s: [String; n]
-    }
-    Input { id, n, k, s }
-}
-
-pub struct Output {
-    pub q: usize,
-    pub yxc: Vec<(usize, usize, usize)>,
-}
-
-pub fn parse_output(f: &str) -> Output {
-    let f = proconio::source::once::OnceSource::from(f);
-    input! {
-        from f,
-        q: usize,
-        yxc: [(usize, usize, usize); q]
-    }
-    Output { q, yxc }
-}
-
-pub fn gen(seed: u64) -> Input {
-    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
-    let id = seed;
-    let n = 100;
-    let k = 9;
-    let s = (0..n)
-        .map(|_| {
-            (0..n)
-                .map(|_| rng.gen_range(1, k + 1).to_string())
-                .collect::<String>()
-        })
-        .collect::<Vec<_>>();
-    Input { id: 0, n, k, s }
-}
-
-fn calculate_score(input: &Input, yxc: &Vec<(usize, usize, usize)>) -> (usize, Vec<Vec<usize>>) {
-    let mut state = vec![vec![0; input.n]; input.n];
-    input.s.iter().enumerate().for_each(|(y, s)| {
-        s.chars()
-            .enumerate()
-            .for_each(|(x, c)| state[y][x] = c.to_digit(10).unwrap() as usize)
-    });
-
-    let x_vec: Vec<i32> = vec![0, 1, 0, -1];
-    let y_vec: Vec<i32> = vec![-1, 0, 1, 0];
-
-    for (y, x, c) in yxc {
-        // state[*y][*x] = *c;
-        let selected_color = state[*y - 1][*x - 1];
-
-        let mut visited = vec![vec![false; input.n]; input.n];
-        let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
-        queue.push_back((*y - 1, *x - 1));
-
-        let mut count = 0;
-
-        while queue.len() > 0 {
-            let (ypos, xpos) = queue.pop_front().unwrap();
-            if visited[ypos][xpos] {
-                continue;
-            }
-            visited[ypos][xpos] = true;
-            state[ypos][xpos] = *c;
-
-            count = count + 1;
-            for i in 0..4 {
-                let nx = xpos as i32 + x_vec[i];
-                let ny = ypos as i32 + y_vec[i];
-                if nx < 0 || ny < 0 || nx >= input.n as i32 || ny >= input.n as i32 {
-                    continue;
-                }
-                let nx = nx as usize;
-                let ny = ny as usize;
-                if visited[ny][nx] {
-                    continue;
-                }
-
-                if state[ny][nx] != selected_color {
-                    continue;
-                }
-                queue.push_back((ny, nx));
-            }
-        }
-    }
-
-    let mut score = 0;
-    for color in 1..(input.k + 1) {
-        let mut tmp_score = 0;
-        for y in 0..input.n {
-            for x in 0..input.n {
-                if state[y][x] == color {
-                    tmp_score += 100;
-                }
-            }
-        }
-        score = score.max(tmp_score);
-    }
-    score -= yxc.len();
-
-    return (score, state);
-}
-
-fn generate_dark_color(code: usize) -> String {
-    // 入力値に基づいてHue（色相）を計算
-    let hue = (code as f32 * 36.0) % 360.0;
-
-    // Saturation（彩度）を低めに、Lightness（明度）を固定値で低く設定
-    let saturation = 30.0;
-    let lightness = 30.0;
-
-    // HSL to RGB 変換
-    let hue_normalized = hue / 360.0;
-    let q = if lightness < 0.5 {
-        lightness * (1.0 + saturation)
+/// 0 <= val <= 1
+pub fn color(mut val: f64) -> String {
+    val.setmin(1.0);
+    val.setmax(0.0);
+    let (r, g, b) = if val < 0.5 {
+        let x = val * 2.0;
+        (
+            30. * (1.0 - x) + 144. * x,
+            144. * (1.0 - x) + 255. * x,
+            255. * (1.0 - x) + 30. * x,
+        )
     } else {
-        lightness + saturation - (lightness * saturation)
+        let x = val * 2.0 - 1.0;
+        (
+            144. * (1.0 - x) + 255. * x,
+            255. * (1.0 - x) + 30. * x,
+            30. * (1.0 - x) + 70. * x,
+        )
     };
-
-    let p = 2.0 * lightness - q;
-
-    let r = hue_to_rgb(p, q, hue_normalized + 1.0 / 3.0);
-    let g = hue_to_rgb(p, q, hue_normalized);
-    let b = hue_to_rgb(p, q, hue_normalized - 1.0 / 3.0);
-
-    // RGB を 16 進数に変換して文字列を返す
-    format!(
-        "#{:02X}{:02X}{:02X}",
-        (r * 255.0) as u8,
-        (g * 255.0) as u8,
-        (b * 255.0) as u8
-    )
-}
-
-fn generate_color(code: usize) -> String {
-    // 入力値に基づいてHue（色相）を計算
-    let hue = (code as f32 * 36.0) % 360.0;
-
-    // Saturation（彩度）とLightness（明度）を固定値で設定
-    let saturation = 10.0;
-    let lightness = 0.1;
-
-    // HSL to RGB 変換
-    let hue_normalized = hue / 360.0;
-    let q = if lightness < 0.5 {
-        lightness * (1.0 + saturation)
-    } else {
-        lightness + saturation - (lightness * saturation)
-    };
-
-    let p = 2.0 * lightness - q;
-
-    let r = hue_to_rgb(p, q, hue_normalized + 1.0 / 3.0);
-    let g = hue_to_rgb(p, q, hue_normalized);
-    let b = hue_to_rgb(p, q, hue_normalized - 1.0 / 3.0);
-
-    // RGB を 16 進数に変換して文字列を返す
-    format!(
-        "#{:02X}{:02X}{:02X}",
-        (r * 255.0) as u8,
-        (g * 255.0) as u8,
-        (b * 255.0) as u8
-    )
-}
-
-fn hue_to_rgb(p: f32, q: f32, t: f32) -> f32 {
-    let t = if t < 0.0 {
-        t + 1.0
-    } else if t > 1.0 {
-        t - 1.0
-    } else {
-        t
-    };
-
-    if t < 1.0 / 6.0 {
-        p + (q - p) * 6.0 * t
-    } else if t < 1.0 / 2.0 {
-        q
-    } else if t < 2.0 / 3.0 {
-        p + (q - p) * (2.0 / 3.0 - t) * 6.0
-    } else {
-        p
-    }
+    format!("#{:02x}{:02x}{:02x}", r.round() as i32, g.round() as i32, b.round() as i32)
 }
 
 pub fn rect(x: usize, y: usize, w: usize, h: usize, fill: &str) -> Rectangle {
@@ -243,16 +70,202 @@ pub fn rect(x: usize, y: usize, w: usize, h: usize, fill: &str) -> Rectangle {
         .set("width", w)
         .set("height", h)
         .set("fill", fill)
+        // .set("stroke","white")
+        .set("stroke-width",1)
+}
+pub fn txt(x: usize, y: usize, text: &str) -> Text {
+    Text::new()
+        .add(TextContent::new(text))
+        .set("x", x)
+        .set("y", y)
+        .set("fill", "black")
+        .set("font-size", 20)
+        .set("dominant-baseline", "central") // 上下中央揃え
+        .set("text-anchor", "middle") // 左右中央揃え
 }
 
-pub fn vis(input: &Input, output: &Output, turn: usize) -> (i64, String, String) {
-    let (score, state) =
-        calculate_score(input, &output.yxc[0..turn].into_iter().cloned().collect());
+pub fn circle(x: usize, y: usize, r: usize, stroke: &str) -> Circle {
+    Circle::new()
+        .set("cx", x)
+        .set("cy", y)
+        .set("r", r)
+        .set("stroke", stroke)
+        .set("fill","transparent")
+        .set("stroke-width",3)
+}
+pub fn line(x1: usize, y1: usize, x2: usize, y2: usize, color: &str) -> Line {
+    Line::new()
+        .set("x1", x1)
+        .set("y1", y1)
+        .set("x2", x2)
+        .set("y2", y2)
+        .set("stroke", color)
+        .set("stroke-width", 3)
+        .set("stroke-linecap", "round")
+}
 
+pub fn group(title: String) -> Group {
+    Group::new().add(Title::new().add(TextContent::new(title)))
+}
+
+pub fn read<T: Copy + PartialOrd + std::fmt::Display + std::str::FromStr, R: RangeBounds<T>>(
+    token: Option<&str>,
+    range: R,
+) -> Result<T, String> {
+    if let Some(v) = token {
+        if let Ok(v) = v.parse::<T>() {
+            if !range.contains(&v) {
+                Err(format!("Out of range: {}", v))
+            } else {
+                Ok(v)
+            }
+        } else {
+            Err(format!("Parse error: {}", v))
+        }
+    } else {
+        Err("Unexpected EOF".to_owned())
+    }
+}
+
+const DIRS: [char; 4] = ['U', 'D', 'L', 'R'];
+const DIJ: [(usize, usize); 4] = [(!0, 0), (1, 0), (0, !0), (0, 1)];
+
+pub fn gen(seed: u64) -> Input {
+    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
+    let n = 200;
+    let m = 10;
+    let mut is = (1..=n).collect_vec();
+    is.shuffle(&mut rng);
+    let mut bs = vec![vec![]; m];
+    for i in 0..n {
+        bs[i % m].push(is[i]);
+    }
+    Input { n, m, bs }
+}
+
+
+// Todo1 Input 関連の定義
+
+#[derive(Clone, Debug)]
+pub struct Input {
+    pub n: usize,
+    pub m: usize,
+    pub bs: Vec<Vec<usize>>,
+}
+
+impl std::fmt::Display for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{} {}", self.n, self.m)?;
+        for i in 0..self.m {
+            writeln!(f, "{}", self.bs[i].iter().join(" "))?;
+        }
+        Ok(())
+    }
+}
+
+pub fn parse_input(f: &str) -> Input {
+    let f = proconio::source::once::OnceSource::from(f);
+    input! {
+        from f,
+        n: usize, m: usize,
+        bs: [[usize; n / m]; m]
+    }
+    Input { n, m, bs }
+}
+
+// Todo2 Output 関連の定義
+pub struct Output {
+    pub out: Vec<(usize, usize)>,
+}
+
+pub fn parse_output(input: &Input, f: &str) -> Result<Output, String> {
+    let mut out = vec![];
+    let mut tokens = f.split_whitespace().peekable();
+    while tokens.peek().is_some() {
+        out.push((read(tokens.next(), 1..=input.n)?, read(tokens.next(), 0..=input.m)?));
+    }
+    if out.len() > 5000 {
+        return Err("Too many output".to_owned());
+    }
+    Ok(Output { out })
+}
+
+pub fn compute_score(input: &Input, out: &Output) -> (i64, String) {
+    let (mut score, err, _) = compute_score_details(input, &out.out,out.out.len());
+    if err.len() > 0 {
+        score = 0;
+    }
+    (score, err)
+}
+
+pub fn compute_score_details(input: &Input,
+    out: &[(usize,usize)],turn: usize
+) -> (i64, String, Vec<Vec<usize>>) {
+    
+    let mut bs = input.bs.clone();
+    let mut cost = 0;
+    let mut t = 0;
+    let mut trn = 0;
+    for &(v, mut to) in out {
+        if trn == turn {
+            break;
+        }
+        trn += 1;
+        let (i, j) = 'ij: {
+            for i in 0..bs.len() {
+                for j in 0..bs[i].len() {
+                    if bs[i][j] == v {
+                        break 'ij (i, j);
+                    }
+                }
+            }
+            return (0, format!("Box {v} has already been taken out."), bs);
+        };
+        if to == 0 {
+            if j + 1 != bs[i].len() {
+                return (0, format!("Box {v} is not at the top of the stack."), bs);
+            } else if v != t + 1 {
+                return (
+                    0,
+                    format!("Before carrying out box {v}, all boxes less than {v} must be carried out."),
+                    bs,
+                );
+            }
+            bs[i].pop();
+            t += 1;
+        } else {
+            cost += bs[i].len() - j + 1;
+            to -= 1;
+            if i != to {
+                for k in j..bs[i].len() {
+                    let b = bs[i][k];
+                    bs[to].push(b);
+                }
+                bs[i].truncate(j);
+            }
+        }
+    }
+    let score = (10000 - cost as i64).max(1);
+    let err = if t < input.n {
+        format!("Not finished ({} / {})", t, input.n)
+    } else {
+        String::new()
+    };
+    (score, err, bs)
+
+}
+
+// Todo3 vis,score 関連の定義
+pub fn vis(input: &Input, output: &Output, turn: usize) -> (i64, String, String) {
+    let (score, err,bs) =
+    compute_score_details(&input, &output.out,turn);
     let W = 800;
     let H = 800;
-    let w = 8;
-    let h = 8;
+
+    let max_h = (2 * input.n / input.m).max(bs.iter().map(|b| b.len()).max().unwrap());
+    let w = W / input.m;
+    let h = H / max_h;
+
     let mut doc = svg::Document::new()
         .set("id", "vis")
         .set("viewBox", (-5, -5, W + 10, H + 10))
@@ -262,24 +275,133 @@ pub fn vis(input: &Input, output: &Output, turn: usize) -> (i64, String, String)
 
     doc = doc.add(Style::new(format!(
         "text {{text-anchor: middle;dominant-baseline: central; font-size: {}}}",
-        6
+        16 * 2 * input.n / input.m / max_h
     )));
-    for y in 0..input.n {
-        for x in 0..input.n {
+
+    // (score, err, doc.to_string())
+    
+    for i in 0..input.m {
+        for j in 0..bs[i].len() {
             doc = doc.add(
                 rect(
-                    x * w,
-                    W - (y + 1) * h,
-                    w,
+                    i * w + 1,
+                    (max_h - j - 1) * h,
+                    w - 2,
                     h,
-                    &generate_dark_color(state[y][x]),
+                    &color(bs[i][j] as f64 / input.n as f64)
                 )
-                .set("stroke", "black")
-                .set("stroke-width", 1)
-                .set("class", "box"),
+            );
+            doc = doc.add(
+                txt(
+                    i * w + w / 2, 
+                    (max_h - j - 1) * h + h/2,
+                    &bs[i][j].to_string())
             );
         }
     }
 
-    (score as i64, "".to_string(), doc.to_string())
+    (score, err, doc.to_string())
+
+    // let D = 600 / input.N;
+    // let W = D * input.N;
+    // let H = D * input.N;
+    // let mut doc = svg::Document::new()
+    //     .set("id", "vis")
+    //     .set("viewBox", (-5, -5, W + 10, H + 10))
+    //     .set("width", W + 10)
+    //     .set("height", H + 10)
+    //     .set("style", "background-color:white");
+
+    // doc = doc.add(Style::new(format!(
+    //     "text {{text-anchor: middle;dominant-baseline: central; font-size: {}}}",
+    //     6
+    // )));
+    // for x in 0..input.N {
+    //     for y in 0..input.N {
+    //         let c = 1.0 - (b[x][y] as f64 / MOD as f64).powi(3);
+    //         doc = doc.add(
+    //             rect(
+    //                 y * D,
+    //                 x * D,
+    //                 D,
+    //                 D,
+    //                 &color(c),
+    //             )
+    //         );
+    //         doc = doc.add(
+    //             txt(
+    //                 y * D + D / 2, 
+    //                 x * D + D / 2,
+    //                 &b[x][y].to_string()
+    //             )
+    //             .set("font-size",D/6)
+    //         );
+    //     }
+    // }
+    // if turn > 0 {
+    //     let (_,x,y) = &output.out[turn-1];
+    //     doc = doc.add(
+    //         rect(
+    //             y * D,
+    //             x * D,
+    //             3 * D,
+    //             3 * D,
+    //             "none",
+    //         )
+    //         .set("stroke","black")
+    //         .set("stroke-width",3)
+    //     );
+        
+    // }
+
+    
+    // for x in 0..input.N {
+    //     for y in 0..input.N-1 {
+    //         if input.vs[x][y] == '1'{
+    //             doc = doc.add(
+    //                 line(
+    //                     (y+1)*D,
+    //                     x*D,
+    //                     (y+1)*D,
+    //                     (x+1)*D,
+    //                     "black"
+    //                 )
+    //             )
+    //         }
+    //     }
+    // }
+    // for x in 0..input.N-1 {
+    //     for y in 0..input.N {
+    //         if input.hs[x][y] == '1'{
+    //             doc = doc.add(
+    //                 line(
+    //                     y*D,
+    //                     (x+1)*D,
+    //                     (y+1)*D,
+    //                     (x+1)*D,
+    //                     "black"
+    //                 )
+    //             )
+    //         }
+    //     }
+    // }
+    // doc = doc.add(
+    //     circle(
+    //         p1.1 * D + D/2,
+    //         p1.0 * D + D/2,
+    //         D/2,
+    //         "red"
+    //     )
+    // );
+    // doc = doc.add(
+    //     circle(
+    //         p2.1 * D + D/2,
+    //         p2.0 * D + D/2,
+    //         D/2,
+    //         "blue"
+    //     )
+    // );
+
+
+    // (score as i64, "".to_string(), doc.to_string())
 }
